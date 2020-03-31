39674942bee830bb20285803e8c5c1ec83e44f1e
KoSangWon <jackoss@naver.com>
2020-03-31 21:07:03 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 
-const countModifier = () => {
-  return "hello"
+const countModifier = (count = 0, action) => {
+  console.log(action);
+  if(action.type === "ADD"){
+    return count + 1;
+  } else if(action.type === "MINUS"){
+    return count - 1;
+  } else{
+    return count;
+  }
 };
+
 const countStore = createStore(countModifier);
 
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "ADD"})
+countStore.dispatch({type: "MINUS"})
+
 console.log(countStore.getState());
+
