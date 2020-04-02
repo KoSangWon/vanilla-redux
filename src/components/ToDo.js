bab33571c0161951d0b6082d7c79087da1b893ec
KoSangWon <jackoss@naver.com>
2020-04-03 01:12:56 +0900

    #3.5 Detail Screen

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({text, onBtnClick}){
+function ToDo({text, onBtnClick, id}){
     return (
         <li>
+            <Link to={`/${id}`}>
             {text} <button onClick={onBtnClick}>DEL</button>
+            </Link>
         </li>
     )
 }
 
 function mapDispatchToProps(dispatch, ownProps){
     return{
-        onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+        onBtnClick: () => dispatch(actionCreators.deleteToDo(parseInt(ownProps.id)))
     };
 }
 
